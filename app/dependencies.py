428fb83ec85a6837015fd04e8d5e115df1877ec5
Emilio F <emiliofurrer00@gmail.com>
2026-05-31 08:20:57 -0300

    add dependencies file with session cookie related function

---
+++ app/dependencies.py
@@ -0,0 +1,19 @@
+from fastapi import Depends, Request
+from sqlalchemy.orm import Session
+
+from app import crud, models
+from app.database import get_db
+
+def get_current_user_from_cookie(
+    request: Request,
+    db: Session = Depends(get_db),
+) -> models.User | None:
+    user_id = request.cookies.get("user_id")
+
+    if user_id is None:
+        return None
+
+    if not user_id.isdigit():
+        return None
+
+    return crud.get_user_by_id(db, int(user_id))
