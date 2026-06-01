47ee8624ac45c6365d48d833663e8d4897805d81
Emilio F <emiliofurrer00@gmail.com>
2026-06-01 00:51:44 -0300

    small refactor on db related code to follow more modern approaches

--- app/crud.py
+++ app/crud.py
@@ -1,19 +1,19 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app import models
 
 
 def get_user_by_username(db: Session, username: str) -> models.User | None:
-    return db.query(models.User).filter(models.User.username == username).first()
+    statement = select(models.User).where(models.User.username == username)
+    return db.scalar(statement)
 
 
 def get_user_by_id(db: Session, user_id: int) -> models.User | None:
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    statement = select(models.User).where(models.User.id == user_id)
+    return db.scalar(statement)
 
 
 def get_stickers_by_owner(db: Session, owner_id: int) -> list[models.Sticker]:
-    return (
-        db.query(models.Sticker)
-        .filter(models.Sticker.owner_id == owner_id)
-        .all()
-    )
+    statement = select(models.Sticker).where(models.Sticker.owner_id == owner_id)
+    return list(db.scalars(statement))
